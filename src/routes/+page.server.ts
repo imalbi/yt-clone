14a34da9a77386f7ac75a20e1b9800e6cb081617
imalbi <alberto.cerqua@icloud.com>
2025-07-18 14:50:32 +0200

    Add TODO to suggest caching videos in a store
    Notes the need to implement a store to prevent redundant video fetches, aiming to optimize performance and reduce unnecessary API calls in the future.

--- src/routes/+page.server.ts
+++ src/routes/+page.server.ts
@@ -1,5 +1,6 @@
+// src/routes/+page.server.ts
 import * as apis from '$lib/api/youtube';
-
+//TODO: add a store for videos to avoid fetching them multiple times
 export async function load({}) {
 	const videosPromise = apis.getVideos();
 	return { videos: videosPromise, error: null };
